096bc023a03a4070fd1500883668bcd6a41da0cf
Pier Luc Ducharme <dutch80@gmail.com>
2025-11-19 12:54:46 -0500

    ajout exemples semaine 11

--- Semaine 11/Exemples/exemple_10_12.py
+++ Semaine 11/Exemples/exemple_10_12.py
@@ -2,13 +2,13 @@ import csv
 
 ma_matrice = [[0.1, 2.3, 4.5], [6.7, 8.9, 10]]
 
-# Code du livre, problème d'ajout de lignes supplémentaires
-# with open("sauvegarde.csv", mode="w") as fichier:
-#     ecrivain = csv.writer(fichier)
-#     ecrivain.writerows(ma_matrice)
+# Code du livre, tous les lignes à la fois. Corrigé pour enlever le problème de lignes supplémentaires
+with open("sauvegarde.csv", mode="w", encoding="utf8", newline="") as fichier:
+    ecrivain = csv.writer(fichier)
+    ecrivain.writerows(ma_matrice)
 
-# Code corrigé
-with open("sauvegarde.csv", mode="w") as fichier:
+# Code corrigé, version ligne par ligne
+with open("sauvegarde.csv", mode="w", encoding="utf8", newline="") as fichier:
     ecrivain = csv.writer(fichier)
     for ligne in ma_matrice:
         ecrivain.writerow(ligne)
